0dbad3ab23e5b82669311f14bbf2c93796140311
Jeff <rubikscubedude@gmail.com>
2019-05-04 15:12:07 -0700

    updated homepage and added CSS and JS

---
+++ web/personalHomepageJavascript.js
@@ -0,0 +1,7 @@
+function changeY() {
+    document.getElementById("yes").innerHTML = "Thank you for agreeing with me";
+}
+
+function changeN() {
+    document.getElementById("no").innerHTML = "Well, I think it's a lot at least";
+}
