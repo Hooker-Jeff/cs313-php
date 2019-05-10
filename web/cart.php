8e5348ecb0c9d6659972b0004570c4d5b7442750
Jeff <rubikscubedude@gmail.com>
2019-05-10 15:04:44 -0700

    edit

--- web/cart.php
+++ web/cart.php
@@ -1,6 +1,17 @@
 <?php
-
 session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Cart</title>
+<link rel="stylesheet" href="store.css">
+</head>
+
+<body>
+
+<?php
 
 while (list ($key, $val) = each ($_SESSION['cart'])) { 
 echo "$key -> $val <br>"; 
@@ -8,3 +19,6 @@ echo "$key -> $val <br>";
 
 
 ?>
+
+</body>
+</html>
