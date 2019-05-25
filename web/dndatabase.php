92f88bbdeadfadc885c5d6326266b1014e298596
Jeff <rubikscubedude@gmail.com>
2019-05-25 16:48:24 -0700

    edited initial database page

--- web/dndatabase.php
+++ web/dndatabase.php
@@ -57,8 +57,13 @@ $qry="SELECT public.character.local_race_id, public.race.race_id FROM public.cha
 
 		<?php
 		
-			
+		/*
 		foreach ($db->query('SELECT * FROM public.character WHERE character_name = $_POST['value'] ') as $row)
+		*/
+
+
+		
+		foreach ($db->query('SELECT * FROM public.character') as $row)
 		{
 			echo '<table style="width:100%" ><tr><th>Character Name</th>';
 			echo '<th>Player Name</th>';
