8006832ecb40f543062fd5aacd848015161e899d
Jeff <rubikscubedude@gmail.com>
2019-06-04 19:31:14 -0700

    edit on D&D database

--- web/character-update.php
+++ web/character-update.php
@@ -91,98 +91,98 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	
 	
 
-	<input type="text" id="str_level" name="str_level"></input>
+	<input type="text" id="str_level" name="str_level" value="<?php foreach ($rows as $row)echo $row['str_level'];?>"></input>
 	<label for="str_level">Strength</label>
 	<br /><br />
 	
-	<input type="text" id="str_bonus" name="str_bonus"></input>
+	<input type="text" id="str_bonus" name="str_bonus" value="<?php foreach ($rows as $row)echo $row['str_bonus'];?>"></input>
 	<label for="str_bonus">Strength Bonus</label>
 	<br /><br />
 
-	<input type="text" id="str_saving" name="str_saving"></input>
+	<input type="text" id="str_saving" name="str_saving" value="<?php foreach ($rows as $row)echo $row['str_saving'];?>"></input>
 	<label for="str_saving">Strength Saving Throw</label>
 	<br /><br />
 
-	<input type="text" id="dex_level" name="dex_level"></input>
+	<input type="text" id="dex_level" name="dex_level" value="<?php foreach ($rows as $row)echo $row['dex_level'];?>"></input>
 	<label for="dex_level">Dexterity</label>
 	<br /><br />
 	
-	<input type="text" id="dex_bonus" name="dex_bonus"></input>
+	<input type="text" id="dex_bonus" name="dex_bonus" value="<?php foreach ($rows as $row)echo $row['dex_bonus'];?>"></input>
 	<label for="dex_bonus">Dexterity Bonus</label>
 	<br /><br />
 
-	<input type="text" id="dex_saving" name="dex_saving"></input>
+	<input type="text" id="dex_saving" name="dex_saving" value="<?php foreach ($rows as $row)echo $row['dex_saving'];?>"></input>
 	<label for="dex_saving">Dexterity Saving Throw</label>
 	<br /><br />
 	
-	<input type="text" id="con_level" name="con_level"></input>
+	<input type="text" id="con_level" name="con_level" value="<?php foreach ($rows as $row)echo $row['con_level'];?>"></input>
 	<label for="con_level">Constitution</label>
 	<br /><br />
 	
-	<input type="text" id="con_bonus" name="con_bonus"></input>
+	<input type="text" id="con_bonus" name="con_bonus" value="<?php foreach ($rows as $row)echo $row['con_bonus'];?>"></input>
 	<label for="con_bonus">Constitution Bonus</label>
 	<br /><br />
 
-	<input type="text" id="con_saving" name="con_saving"></input>
+	<input type="text" id="con_saving" name="con_saving" value="<?php foreach ($rows as $row)echo $row['con_saving'];?>"></input>
 	<label for="con_saving">Constitution Saving Throw</label>
 	<br /><br />
 	
-	<input type="text" id="int_level" name="int_level"></input>
+	<input type="text" id="int_level" name="int_level" value="<?php foreach ($rows as $row)echo $row['int_level'];?>"></input>
 	<label for="int_level">Intelligence</label>
 	<br /><br />
 	
-	<input type="text" id="int_bonus" name="int_bonus"></input>
+	<input type="text" id="int_bonus" name="int_bonus" value="<?php foreach ($rows as $row)echo $row['int_bonus'];?>"></input>
 	<label for="int_bonus">Intelligence Bonus</label>
 	<br /><br />
 
-	<input type="text" id="int_saving" name="int_saving"></input>
+	<input type="text" id="int_saving" name="int_saving" value="<?php foreach ($rows as $row)echo $row['int_saving'];?>"></input>
 	<label for="int_saving">Intelligence Saving Throw</label>
 	<br /><br />
 	
-	<input type="text" id="wis_level" name="wis_level"></input>
+	<input type="text" id="wis_level" name="wis_level" value="<?php foreach ($rows as $row)echo $row['wis_level'];?>"></input>
 	<label for="wis_level">Wisdom</label>
 	<br /><br />
 	
-	<input type="text" id="wis_bonus" name="wis_bonus"></input>
+	<input type="text" id="wis_bonus" name="wis_bonus" value="<?php foreach ($rows as $row)echo $row['wis_bonus'];?>"></input>
 	<label for="wis_bonus">Wisdom Bonus</label>
 	<br /><br />
 
-	<input type="text" id="wis_saving" name="wis_saving"></input>
+	<input type="text" id="wis_saving" name="wis_saving" value="<?php foreach ($rows as $row)echo $row['wis_saving'];?>"></input>
 	<label for="wis_saving">Wisdom Saving Throw</label>
 	<br /><br />
 	
-	<input type="text" id="cha_level" name="cha_level"></input>
+	<input type="text" id="cha_level" name="cha_level" value="<?php foreach ($rows as $row)echo $row['cha_level'];?>"></input>
 	<label for="cha_level">Charisma</label>
 	<br /><br />
 	
-	<input type="text" id="cha_bonus" name="cha_bonus"></input>
+	<input type="text" id="cha_bonus" name="cha_bonus" value="<?php foreach ($rows as $row)echo $row['cha_bonus'];?>"></input>
 	<label for="cha_bonus">Charisma Bonus</label>
 	<br /><br />
 
-	<input type="text" id="cha_saving" name="cha_saving"></input>
+	<input type="text" id="cha_saving" name="cha_saving" value="<?php foreach ($rows as $row)echo $row['cha_saving'];?>"></input>
 	<label for="cha_saving">Charisma Saving Throw</label>
 	<br /><br /><br /><br />
 	
 	
 	
 	
-	<input type="text" id="ac" name="ac"></input>
+	<input type="text" id="ac" name="ac" value="<?php foreach ($rows as $row)echo $row['ac'];?>"></input>
 	<label for="ac">Armor Class</label>
 	<br /><br />
 	
-	<input type="text" id="hit_dice" name="hit_dice"></input>
+	<input type="text" id="hit_dice" name="hit_dice" value="<?php foreach ($rows as $row)echo $row['hit_dice'];?>"></input>
 	<label for="hit_dice">Total Hit Dice</label>
 	<br /><br />
 
-	<input type="text" id="prof" name="prof"></input>
+	<input type="text" id="prof" name="prof" value="<?php foreach ($rows as $row)echo $row['prof_bonus'];?>"></input>
 	<label for="prof">Proficiency Bonus</label>
 	<br /><br />
 
-	<input type="text" id="speed" name="speed"></input>
+	<input type="text" id="speed" name="speed" value="<?php foreach ($rows as $row)echo $row['speed'];?>"></input>
 	<label for="speed">Character Speed</label>
 	<br /><br />
 	
-	<input type="text" id="gold" name="gold"></input>
+	<input type="text" id="gold" name="gold" value="<?php foreach ($rows as $row)echo $row['gold'];?>"></input>
 	<label for="gold">Character Gold</label>
 	<br /><br /><br /><br />
 	
@@ -190,75 +190,75 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	
 	
 
-	<input type="text" id="athletics" name="athletics"></input>
+	<input type="text" id="athletics" name="athletics" value="<?php foreach ($rows as $row)echo $row['ability1'];?>"></input>
 	<label for="athletics">Athletics</label>
 	<br /><br />
 
-	<input type="text" id="acrobatics" name="acrobatics"></input>
+	<input type="text" id="acrobatics" name="acrobatics" value="<?php foreach ($rows as $row)echo $row['ability2'];?>"></></input>
 	<label for="acrobatics">Acrobatics</label>
 	<br /><br />
 	
-	<input type="text" id="slight" name="slight"></input>
+	<input type="text" id="slight" name="slight" value="<?php foreach ($rows as $row)echo $row['ability3'];?>"></></input>
 	<label for="slight">Slight of Hand</label>
 	<br /><br />
 
-	<input type="text" id="stealth" name="stealth"></input>
+	<input type="text" id="stealth" name="stealth" value="<?php foreach ($rows as $row)echo $row['ability4'];?>"></></input>
 	<label for="stealth">Stealth</label>
 	<br /><br />
 
-	<input type="text" id="arcane" name="arcane"></input>
+	<input type="text" id="arcane" name="arcane" value="<?php foreach ($rows as $row)echo $row['ability5'];?>"></></input>
 	<label for="arcane">Arcane</label>
 	<br /><br />
 	
-	<input type="text" id="history" name="history"></input>
+	<input type="text" id="history" name="history" value="<?php foreach ($rows as $row)echo $row['ability6'];?>"></></input>
 	<label for="history">History</label>
 	<br /><br />
 
-	<input type="text" id="investigation" name="investigation"></input>
+	<input type="text" id="investigation" name="investigation" value="<?php foreach ($rows as $row)echo $row['ability7'];?>"></></input>
 	<label for="investigation">Investigation</label>
 	<br /><br />
 
-	<input type="text" id="nature" name="nature"></input>
+	<input type="text" id="nature" name="nature" value="<?php foreach ($rows as $row)echo $row['ability8'];?>"></></input>
 	<label for="nature">Nature</label>
 	<br /><br />
 	
-	<input type="text" id="religion" name="religion"></input>
+	<input type="text" id="religion" name="religion" value="<?php foreach ($rows as $row)echo $row['ability9'];?>"></></input>
 	<label for="religion">Religion</label>
 	<br /><br />
 
-	<input type="text" id="insight" name="insight"></input>
+	<input type="text" id="insight" name="insight" value="<?php foreach ($rows as $row)echo $row['ability10'];?>"></></input>
 	<label for="insight">Insight</label>
 	<br /><br />
 
-	<input type="text" id="medicine" name="medicine"></input>
+	<input type="text" id="medicine" name="medicine" value="<?php foreach ($rows as $row)echo $row['ability11'];?>"></></input>
 	<label for="medicine">Medicine</label>
 	<br /><br />
 	
-	<input type="text" id="animal" name="animal"></input>
+	<input type="text" id="animal" name="animal" value="<?php foreach ($rows as $row)echo $row['ability12'];?>"></></input>
 	<label for="animal">Animal Handling</label>
 	<br /><br />
 
-	<input type="text" id="percep" name="percep"></input>
+	<input type="text" id="percep" name="percep" value="<?php foreach ($rows as $row)echo $row['ability13'];?>"></></input>
 	<label for="percep">Perception</label>
 	<br /><br />
 
-	<input type="text" id="survival" name="survival"></input>
+	<input type="text" id="survival" name="survival" value="<?php foreach ($rows as $row)echo $row['ability14'];?>"></></input>
 	<label for="survival">Survival</label>
 	<br /><br />
 	
-	<input type="text" id="deception" name="deception"></input>
+	<input type="text" id="deception" name="deception" value="<?php foreach ($rows as $row)echo $row['ability15'];?>"></></input>
 	<label for="deception">Deception</label>
 	<br /><br />
 
-	<input type="text" id="intimidation" name="intimidation"></input>
+	<input type="text" id="intimidation" name="intimidation" value="<?php foreach ($rows as $row)echo $row['ability16'];?>"></></input>
 	<label for="intimidation">Intimidation</label>
 	<br /><br />
 
-	<input type="text" id="performance" name="performance"></input>
+	<input type="text" id="performance" name="performance" value="<?php foreach ($rows as $row)echo $row['ability17'];?>"></></input>
 	<label for="performance">Performance</label>
 	<br /><br />
 
-	<input type="text" id="persuasion" name="persuasion"></input>
+	<input type="text" id="persuasion" name="persuasion" value="<?php foreach ($rows as $row)echo $row['ability18'];?>"></></input>
 	<label for="persuasion">Persuasion</label>
 	<br /><br />
 
