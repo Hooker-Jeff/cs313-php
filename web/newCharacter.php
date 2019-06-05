0377af3be35f3cf1dfe2f314d8463769e13e6ac9
Jeff <rubikscubedude@gmail.com>
2019-06-04 20:53:54 -0700

    edit on D&D database

--- web/newCharacter.php
+++ web/newCharacter.php
@@ -49,15 +49,17 @@ $db = get_db();
 	<input type="number" id="race_id" name="race_id" min="10" max="18"></input>
 	<label for="race_id">Race ID</label>
 	<br />
-	<p>(10-Dwarf, 11-Elf, 12-Halfling, 13-Human, 14-Dragonborn, 15-Gnome, 16-Half Elf, 17-Half Ork, 18-Tiefling)</p><br /><br />
+	<p>(10-Dwarf, 11-Elf, 12-Halfling, 13-Human, 14-Dragonborn, 15-Gnome, 16-Half Elf, 17-Half Ork, 18-Tiefling)</p><br />
 	
 	<input type="number" id="class_id" name="class_id" min="20" max="31"></input>
 	<label for="class_id">Class ID</label>
-	<br /><br />
+	<br />
+	<p>(20-Barbarian, 21-Bard, 22-Cleric, 23-Druid, 24-Fighter, 25-Monk, 26-Paladin, 27-Ranger, 28-Rogue, 29-Sorcerer, 30-Warlock, 31-Wizard)</p><br />
 	
 	<input type="number" id="alignment_id" name="alignment_id" min="40" max="48"></input>
 	<label for="alignment_id">Alignment ID</label>
-	<br /><br />
+	<br />
+	<p>(40-Lawful Good, 41-Lawful Neutral, 42-Lawful Evil, 43-Neutral Good, 44-True Neutral, 45-Neutral Evil, 46-Chaotic Good, 47-Chaotic Neutral, 48-Chaotic Evil)</p><br />
 
 	<input type="text" id="char_level" name="char_level"></input>
 	<label for="char_level">Character Level</label>
