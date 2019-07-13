5d8e7c465bca480def035dc004787e775887effd
Jeff <rubikscubedude@gmail.com>
2019-07-13 11:20:06 -0700

    edit on project 2 html

--- web/inventory-manager.php
+++ web/inventory-manager.php
@@ -41,7 +41,7 @@ $character_name = $character['character_name'];
 
 <?php
 echo '<div id="myDIV" class="header">' . "\n";
-echo '<h1> Inventory for ' . $character['character_name'] . '</h1><br/>' . "\n";
+echo '<h1> Inventory for ' . $character_name . '</h1><br/>' . "\n";
 echo '<input type="text" id="input" placeholder="Item...">' . "\n";
 echo '<span onclick="newElement()" class="addBtn">Add</span>' . "\n";
 echo '</div>' . "\n";
